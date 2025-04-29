3f010c46e4ba8d9f32d684aa3dc3a362e22b0a6a
Waqas Khan <waqaskhan4447770@gmail.com>
2025-04-29 17:50:37 +0530

    Add files via upload

---
+++ MultipleTryCatch.java
@@ -0,0 +1,31 @@
+package exceptionHandling;
+
+
+
+public class MultipleTryCatch 
+{
+	public static void main(String []args) {
+		
+		try
+		{
+			int a=10,b=0,c;
+			c=a/b;
+			System.out.println(c);
+		}
+		catch(ArithmeticException a) 
+		{
+			System.out.println("can't devide by zero");
+		}
+		
+		try
+		{
+			int a[]= {10,20,30,40};
+			System.out.println(a[5]);
+		}
+		catch(ArrayIndexOutOfBoundsException b) 
+		{
+			System.out.println("beyond the array limit");
+		}
+	}
+
+}
